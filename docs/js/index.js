3ad7a5ddbc310d0294d356ffcc7cee21505151ba
sadjehwty <150713+sadjehwty@users.noreply.github.com>
2024-09-25 15:30:53 +0200

    link top

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,3 +2,21 @@ document.getElementById('email').addEventListener('click',function(){
     let email = document.getElementById('email').dataset['email'];
     location.href='mailto:'+email.replace('[anfora]','@');
 });
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    let anchor=document.getElementById('links');
+    let rect = anchor.getBoundingClientRect();
+    const elemTop = rect.top;
+    if( elemTop < 0){
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
